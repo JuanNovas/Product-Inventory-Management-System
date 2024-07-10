4814c017c7e9dd6305cec88fa864b10acbefd690
Juan Novas <juanignacionovas@gmail.com>
2024-07-10 10:23:26 -0300

    stock update on sale

--- database/init__db.sql
+++ database/init__db.sql
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS products (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     price INTEGER NOT NULL DEFAULT 0 CHECK (price > 0),
-    stock INTEGER NOT NULL DEFAULT 0 CHECK (stock > 0),
+    stock INTEGER NOT NULL DEFAULT 0 CHECK (stock >= 0),
     category_id INTEGER REFERENCES categories(id),
     producer_id INTEGER REFERENCES producers(id)
 );
@@ -24,6 +24,20 @@ CREATE TABLE IF NOT EXISTS sales (
     amount INTEGER DEFAULT 1 CHECK (amount > 0),
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE OR REPLACE FUNCTION update_stock_on_sale()
+RETURNS TRIGGER AS $$
+BEGIN
+    UPDATE products SET stock = stock - NEW.amount
+    WHERE id = NEW.product_id;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_stock_on_sale_trigger
+BEFORE INSERT ON sales
+FOR EACH ROW
+EXECUTE FUNCTION update_stock_on_sale();
 
 BEGIN;
 
